2a2e0d4ca95beb22a68b1ed0aa5b09e4217bd1fe
disa6302 <disa6302@colorado.edu>
2020-08-05 10:00:46 -0700

    Sample master and viewer app

---
+++ webrtc-c/canary/src/KvsWebRtcViewerCanary.cpp
@@ -0,0 +1,33 @@
+#include "Include_i.h"
+
+STATUS run(INT32 argc, CHAR* argv[])
+{
+    STATUS retStatus = STATUS_SUCCESS;
+    Canary::Config config;
+
+    CHK_STATUS(Canary::Config::init(argc, argv, &config));
+    CHK_STATUS(Canary::Cloudwatch::init(&config));
+    CHK_STATUS(initKvsWebRtc());
+
+    DLOGE("Test log");
+
+CleanUp:
+
+    Canary::Cloudwatch::deinit();
+
+    return retStatus;
+}
+
+INT32 main(INT32 argc, CHAR* argv[])
+{
+    STATUS retStatus = STATUS_SUCCESS;
+
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+
+    CHK_STATUS(run(argc, argv));
+
+
+CleanUp:
+    Aws::ShutdownAPI(options);
+}
